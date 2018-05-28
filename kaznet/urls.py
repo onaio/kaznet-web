f85af445cda4f4d76d0edcd8d9312a43e4284459
DavisRayM <davisraymondmuro@outlook.com>
2018-05-28 15:03:03 +0300

    Fix Pylint Error

--- kaznet/urls.py
+++ kaznet/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# pylint: disable=invalid-name
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ona/', include('kaznet.apps.ona.urls'))
