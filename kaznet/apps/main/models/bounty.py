5b0b37cbf0d70c52c532309b5b41fe0ddbd9cf74
Njagi Mwaniki <njagi@urbanslug.com>
2018-05-29 18:30:14 +0300

    Added bounty model
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

---
+++ kaznet/apps/main/models/bounty.py
@@ -0,0 +1,42 @@
+"""
+Module for the Bounty model(s)
+"""
+from django.db import models
+from django.utils.translation import ugettext as _
+
+from django_prices.models import MoneyField
+from tasking.models.base import TimeStampedModel
+
+from django.conf import settings
+
+class Bounty(TimeStampedModel):
+    modified = None
+
+    task = models.ForeignKey(
+        'main.Task',
+        verbose_name=_('Task'),
+        on_delete=models.CASCADE,
+        help_text=_('The task the bounty is for.'),
+    )
+
+    amount = MoneyField(
+        verbose_name=_('Amount'),
+        decimal_places=2,
+        max_digits=64,
+        default=0,
+        currency=settings.KAZNET_DEFAULT_CURRENCY
+    )
+
+
+    def __str__(self):
+        """
+        String representation of a Task object
+
+        e.g. Task 1 bounty is 5000
+        """
+        return _(f"Task {self.task.id} bounty is {self.amount}")
+
+
+    class Meta(object):
+        ordering=['created', 'id']
+        abstract=False
---
+++ kaznet/apps/main/tests/models/test_bounty.py
@@ -0,0 +1,19 @@
+"""
+Test for Bounty model
+"""
+from django.test import TestCase
+from model_mommy import mommy
+
+class TestBounty(TestCase):
+    """
+    Test class for Bounty mdoels
+    """
+
+    def test_bounty_str(self):
+
+        amount=5000
+        kitten_prices = mommy.make('main.Task', name="kitten prices")
+        bounty = mommy.make('main.Bounty', task=kitten_prices, amount=amount)
+
+        expected = f"Task {kitten_prices.id} bounty is {amount}"
+        self.assertEqual(expected, bounty.__str__())
