cb4a3760d9184c3a7db8769249ed1f779198e3b4
DavisRayM <davisraymondmuro@outlook.com>
2018-05-31 14:39:54 +0300

    Modify Api Module for Ona App - Remove Tests for API Module - Modify methods to make them more independent

--- kaznet/apps/ona/api.py
+++ kaznet/apps/ona/api.py
@@ -1,6 +1,6 @@
 """
-Module containing methods that communicates
-with the Onadata API
+Module containing methods that communicate
+with the OnaData API
 """
 from urllib.parse import urljoin
 
@@ -8,185 +8,13 @@ import dateutil.parser
 import requests
 from requests.adapters import HTTPAdapter
 # pylint: disable=import-error
-from requests.packages.urllib3.util.retry import Retry
+from requests.packages.urllib3.util import retry
 
-from kaznet.apps.ona.models import OnaInstance, OnaProject, XForm
+from kaznet.apps.ona.models import Project, XForm, Instance
 from kaznet.settings.common import ONA_BASE_URL, ONA_PASSWORD, ONA_USERNAME
-from kaznet.apps.ona.exceptions import ImproperMethod, InvalidResponse
 
 
-def get_projects(username: str = ONA_USERNAME):
-    """
-    Custom Method that takes in an Ona Users username and
-    Makes a request to onadata api. Takes the response gotten
-    and Loops through it while passing Project data contained in response
-    to the process_project() method.
-    """
-    args = {'owner': f'{username}'}
-    url = urljoin(ONA_BASE_URL, 'api/v1/projects')
-    projects_data = request(url, args)
-
-    if projects_data != InvalidResponse:
-        for project_data in projects_data:
-            process_project(project_data)
-
-
-def process_project(project_data: dict):
-    """
-    Custom Method that takes a Dictionary containing Project Data and Creates
-    or Updates an OnaProject Object Then It Retrieves the Forms in Project Data
-    and Loops through each item while passing it to the process_xform() method.
-    """
-
-    obj, created = OnaProject.objects.get_or_create(
-        ona_pk=project_data['projectid'],
-        defaults={
-            'name': project_data['name'],
-            'deleted_at': project_data['deleted_at'],
-            'ona_last_updated': project_data['date_modified']
-            }
-        )
-
-    if created is False:
-        # If object was not created this means it exists so we check
-        # if it needs updating or not.
-
-        # Turns the project_data['date_modified'] into a datetime object
-        # for easier comparison
-        mocked_date = dateutil.parser.parse(project_data['date_modified'])
-
-        if obj.ona_last_updated != mocked_date:
-            obj.name = project_data['name']
-            obj.ona_last_updated = project_data['date_modified']
-            obj.deleted_at = project_data['deleted_at']
-            obj.save()
-
-    for xform_data in project_data['forms']:
-        # Creates form
-        process_xform(xform_data, obj.ona_pk)
-
-
-def get_xform(form_id: int, project_obj: object):
-    """
-    Custom Method that takes in a form id from ona and a OnaProject Object.
-    Requests data for the specific formid and creates or updates an XForm
-    Object, Then it passes the created/updated obj to get_instances().
-    """
-    url = urljoin(ONA_BASE_URL, f'api/v1/forms/{form_id}')
-    xform_data = request(url)
-
-    if xform_data != InvalidResponse:
-        obj, created = XForm.objects.get_or_create(
-            ona_pk=xform_data['formid'],
-            defaults={
-                'title': xform_data['title'],
-                'id_string': xform_data['id_string'],
-                'ona_last_updated': xform_data['last_updated_at'],
-                'ona_project_id': project_obj.ona_pk,
-                }
-            )
-
-        if created is False:
-            # If object was not created this means it exists so we check
-            # if it needs updating or not.
-
-            mocked_date = dateutil.parser.parse(xform_data['last_updated_at'])
-            if obj.ona_last_updated != mocked_date:
-                obj.title = xform_data['title']
-                obj.id_string = xform_data['id_string']
-                obj.ona_last_updated = xform_data['last_updated_at']
-                obj.save()
-
-        get_instances(obj)
-
-
-def process_xform(xform_data: dict, project_obj_pk: int):
-    """
-    Custom Method thats takes in a Dictionary containing XForm Data
-    and a OnaProject Object. Requests data for the specific formid
-    and creates or updates an XForm Object, Then it passes the XForm
-    object to get_instances().
-    """
-    obj, created = XForm.objects.get_or_create(
-        ona_pk=xform_data['formid'],
-        defaults={
-            'title': xform_data['name'],
-            'id_string': xform_data['id_string'],
-            'ona_project_id': project_obj_pk
-            }
-        )
-
-    if created is False:
-        # If object was not created this means it exists so we check
-        # if it needs updating or not.
-
-        # We check using title of form since that is the only changeable
-        # field returned in response
-        if obj.title != xform_data['name']:
-            obj.title = xform_data['name']
-            obj.id_string = xform_data['id_string']
-            obj.save()
-
-    get_instances(obj)
-
-
-def get_instances(xform: object):
-    """
-    Custom Method that takes in an XForm Object and Retrieves
-    Its Instances/Data from the OnaData Api. Then, it loops
-    through the received Instances/Data and passes each individual
-    Instance/Data to process_instance()
-    """
-    xformid = xform.ona_pk
-    end_page = None
-    start = 0
-
-    while end_page is None:
-        url = urljoin(ONA_BASE_URL, f'api/v1/data/{xformid}')
-        args = {'start': start, 'limit': '100'}
-        instances_data = request(url, args)
-
-        if instances_data != InvalidResponse:
-            if instances_data == []:
-                end_page = True
-                break
-
-            for instance_data in instances_data:
-                process_instance(instance_data, xform)
-
-            start = start + 100
-
-
-def process_instance(instance_data: dict, xform: object):
-    """
-    Custome Method that takes in a Dictionary containing Instances
-    Data and an XForm Object. It Creates or Updates an OnaInstance
-    Object using the inputs.
-    """
-    obj, created = OnaInstance.objects.get_or_create(
-        ona_pk=instance_data['_id'],
-        defaults={
-            'xform': xform,
-            'json': instance_data
-            }
-        )
-
-    if created is False:
-        # If object was not created this means it exists so we check
-        # if it needs updating or not.
-        if instance_data['_edited'] is True:
-            if obj.edited is not True:
-                obj.ona_last_updated = instance_data['_last_edited']
-                obj.edited = True
-
-            mocked_date = dateutil.parser.parse(instance_data['_last_edited'])
-            if obj.ona_last_updated != mocked_date:
-                obj.json = instance_data
-                obj.ona_last_updated = instance_data['_last_edited']
-                obj.save()
-
-
-def requests_session(
+def request_session(
         url: str,
         method: str,
         payload: dict = None,
@@ -200,44 +28,222 @@ def requests_session(
     Session and Retry Object and mounts a HTTP Adapter to the
     Session and Sends a request to the url. It then returns the Response.
     """
+    rets = retry.Retry
     session = requests.Session()
-    retry = Retry(
+    retries = rets(
         total=retries,
         read=retries,
         connect=retries,
         backoff_factor=backoff_factor,
         status_forcelist=status_forcelist
     )
-    adapter = HTTPAdapter(max_retries=retry)
+    adapter = HTTPAdapter(max_retries=retries)
     session.mount('https', adapter)
 
     if method == 'GET':
         response = session.get(
             url, auth=(ONA_USERNAME, ONA_PASSWORD), params=payload
             )
+        return response
     elif method == 'POST':
         response = session.post(
             url, auth=(ONA_USERNAME, ONA_PASSWORD), data=payload
             )
+        return response
     else:
-        raise ImproperMethod
-
-    return response
+        response = None
+        return response
 
 
 def request(
         url: str,
         args: dict = None,
-        request: str = 'GET'
+        method: str = 'GET'
 ):
     """
     Custom Method that requests data from requests_session
     and confirms it has a valid JSON return
     """
-    response = requests_session(url, request, args)
+    response = request_session(url, method, args)
 
     try:
-        response.json()
         return response.json()
     except ValueError:
-        return InvalidResponse
+        return None
+    except AttributeError:
+        return None
+
+
+def get_projects(username: str = ONA_USERNAME):
+    """
+    Custom Method that returns all Projects owned
+    by the User from the OnaData API
+    """
+    args = {'owner': username}
+    url = urljoin(ONA_BASE_URL, 'api/v1/projects')
+    projects_data = request(url, args)
+
+    return projects_data
+
+
+def process_projects(projects_data: dict):
+    """
+    Takes a Dictionary containing Data about Projects
+    and Processes each one
+    """
+    if projects_data is not None:
+        for project_data in projects_data:
+            process_project(project_data)
+
+
+def process_project(project_data: dict):
+    """
+    Custom method that takes a projects data and creates
+    an Instance of that Project
+    """
+    projectid = project_data.get('projectid')
+
+    if projectid is not None:
+        obj, created = Project.objects.get_or_create(
+            ona_pk=projectid,
+            defaults={
+                'name': project_data.get('name'),
+                'deleted_at': project_data.get('deleted_at'),
+                'last_updated': project_data.get('date_modified')
+                }
+            )
+
+        if created is False:
+            # If object was not created this means it exists so we check
+            # if it needs updating or not.
+
+            # Turns the project_data['date_modified'] into a datetime object
+            # for easier comparison
+            mocked_date = dateutil.parser.parse(project_data.get(
+                'date_modified'))
+            if mocked_date is not None:
+                if obj.last_updated != mocked_date:
+                    obj.name = project_data.get('name')
+                    obj.last_updated = project_data.get('date_modified')
+                    obj.deleted_at = project_data.get('deleted_at')
+                    obj.save()
+
+
+def process_forms_data(projects_data: dict):
+    """
+    Takes a Dictionary containing Data about Projects
+    and Processes the forms passed in that Dictionary
+    """
+    if projects_data is not None:
+        for project_data in projects_data:
+            forms_data = project_data.get('forms')
+            return forms_data
+
+
+def process_xforms(forms_data: dict, project_id: int):
+    """
+    Takes a Dictionary containing Data about Forms
+    and process each one
+    """
+    if forms_data is not None:
+        for xform_data in forms_data:
+            process_xform(xform_data, project_id)
+
+
+def process_xform(xform_data: dict, project_id: int):
+    """
+    Takes a Dictionary containing Data about an XForm
+    and Creates or Updates that XForm Instance
+    """
+    xformid = xform_data.get('formid')
+
+    if xformid is not None:
+        obj, created = XForm.objects.get_or_create(
+            ona_pk=xformid,
+            defaults={
+                'title': xform_data.get('name'),
+                'id_string': xform_data.get('id_string'),
+                'project_id': project_id
+            }
+            )
+    if created is False:
+        # If object was not created this means it exists so we check
+        # if it needs updating or not.
+
+        # We check using title of form since that is the only changeable
+        # field returned in response
+        if obj.title != xform_data.get('name'):
+            obj.title = xform_data.get('name')
+            obj.id_string = xform_data.get('id_string')
+            obj.save()
+
+
+def get_instances(xform: object):
+    """
+    Takes an XForm Object and Retrieves its Instances from
+    OnaData
+    """
+    xformid = xform.ona_pk
+    end_page = None
+    start = 0
+    instances_data = []
+
+    while end_page is None:
+        url = urljoin(ONA_BASE_URL, f'api/v1/data/{xformid}')
+        args = {'start': start, 'limit': 100}
+        data = request(url, args)
+        start = start + 100
+        import ipdb; ipdb.set_trace()
+        if data == []:
+            end_page = True
+            break
+        elif data is not None:
+            instances_data = instances_data + data
+
+    return instances_data
+
+
+def process_instances(instances_data: dict, xform: object):
+    """
+    Takes a Dictionary Containing Data on Instances
+    and Process them
+    """
+    if instances_data != []:
+        for instance_data in instances_data:
+            import ipdb; ipdb.set_trace()
+            process_instance(instance_data, xform)
+
+
+def process_instance(instance_data: dict, xform: object):
+    """
+    Takes a Dictionary Containong Data on an Instance
+    and Creates or Updates that Instance
+    """
+    instanceid = instance_data.get('_id')
+
+    obj, created = Instance.objects.get_or_create(
+        ona_pk=instanceid,
+        defaults={
+            'xform': xform,
+            'json': instance_data
+        }
+    )
+
+    if created is False:
+        # If object was not created this means it exists so we check
+        data = obj.json
+        edited = data.get('_edited')
+        data_edited = instance_data.get('_edited')
+        last_updated = instance_data.get('_last_edited')
+
+        if edited is not True and data_edited is True:
+            obj.last_updated = instance_data.get('_last_edited')
+            obj.json = instance_data
+            obj.save()
+
+        mocked_date = dateutil.parser.parse(last_updated)
+
+        if obj.last_updated != mocked_date:
+            obj.json = instance_data
+            obj.last_updated = instance_data.get('_last_edited')
+            obj.save()
