4cfad2accd2fc72c1b74777e10a46f8ae90fbe47
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-05-31 16:14:48 +0300

    Add UserProfileViewSet

--- kaznet/apps/users/viewsets.py
+++ kaznet/apps/users/viewsets.py
@@ -1,3 +1,30 @@
 """
 Viewsets for users app
 """
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, mixins, viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from kaznet.apps.users.models import UserProfile
+from kaznet.apps.users.serializers import UserProfileSerializer
+
+
+# pylint: disable=too-many-ancestors
+class UserProfileViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
+                         mixins.RetrieveModelMixin, mixins.DestroyModelMixin,
+                         mixins.UpdateModelMixin, viewsets.GenericViewSet):
+    """
+    ViewSet class for UserProfiles
+    """
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.OrderingFilter,
+        filters.SearchFilter]
+    search_fields = [
+        'first_name', 'last_name', 'ona_username', 'email', 'national_id']
+    ordering_fields = [
+        'first_name', 'last_name', 'submission_count', 'created',
+        'national_id']
+    queryset = UserProfile.objects.first()  # pylint: disable=no-member
