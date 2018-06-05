364485afc8e773f7d94157ed0fe31c2625b1725a
Njagi Mwaniki <njagi@urbanslug.com>
2018-06-05 13:30:21 +0300

    Create kaznet submission via a post save signal
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

--- kaznet/apps/main/signals.py
+++ kaznet/apps/main/signals.py
@@ -6,7 +6,7 @@ from django.db.models.signals import post_save
 from tasking.utils import generate_task_occurrences
 
 from kaznet.apps.main.models import TaskOccurrence
-
+from kaznet.apps.main.models import Submission
 
 # pylint: disable=unused-argument
 def create_occurrences(sender, instance, created, **kwargs):
@@ -20,8 +20,23 @@ def create_occurrences(sender, instance, created, **kwargs):
         generate_task_occurrences(
             instance, OccurrenceModelClass=TaskOccurrence)
 
-def create_submission():
-    pass
+def create_submission(sender, instance, created, **kwargs):
+    """
+    Create a kaznet.apps.main submission after a
+    kaznet.apps.ona submission is created
+    """
+    task = instance.get_task()
+    if task is not None:
+        bounty = task.bounty_set.all().order_by('-created').first()
+        submission = Submission(
+            task=task,
+            bounty=bounty,
+            location=None, # TODO: in different PR
+            # TODO: what if dict lacks some keys
+            submission_time=instance.json["submission_time"],
+            user_id=instance.json["user_id"]
+        )
+        submission.save()
 
 
 post_save.connect(
--- kaznet/apps/main/tests/test_signals.py
+++ kaznet/apps/main/tests/test_signals.py
@@ -7,9 +7,13 @@ from django.test import TestCase
 from model_mommy import mommy
 
 from kaznet.apps.main.models import TaskOccurrence
-
+from kaznet.apps.main.models import Submission
+from django.utils import timezone
 
 class TestSignals(TestCase):
+    """
+    Tests for Kanzet app signals
+    """
     def test_task_occurrences(self):
         """
         Test that task occurrences are created when a new Task object is
@@ -22,3 +26,26 @@ class TestSignals(TestCase):
 
         # pylint: disable=no-member
         self.assertEqual(57, TaskOccurrence.objects.filter(task=task).count())
+
+    def test_create_submissions(self):
+        """
+        Check that a kaznet submission is created when an ona
+        submission is made
+        """
+        ona_form = mommy.make('ona.XForm')
+        user=mommy.make('auth.User')
+        puppy_task=mommy.make(
+            'main.Task',
+            name='Puppy Prices',
+            target_content_object = ona_form
+        )
+        bounty = mommy.make('main.Bounty', task=puppy_task)
+        submission = mommy.make(
+            'ona.OnaInstance',
+            xform=ona_form,
+            # TODO: make sure JSON data from ONA is saved in a consistent way
+            # TODO: add user field to ona submission
+            json=dict(submission_time=timezone.now().isoformat(), user_id=user.id)
+        )
+
+        self.assertEqual(1, Submission.objects.filter(task=puppy_task).count())
