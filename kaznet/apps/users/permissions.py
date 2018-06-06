418e73a6fbe12e4f10c504e77b8fe26f31ce5ec1
DavisRayM <davisraymondmuro@outlook.com>
2018-06-06 11:18:13 +0300

    Add New Permissions

--- kaznet/apps/users/permissions.py
+++ kaznet/apps/users/permissions.py
@@ -6,7 +6,19 @@ from rest_framework import permissions
 from kaznet.apps.users.models import UserProfile
 
 
-class AdminUserPermission(permissions.BasePermission):
+def check_admin_permission(request: object):
+    """
+    Checks if the user sending a request is an Admin
+    """
+    try:
+        return request.user.userprofile.role == UserProfile.ADMIN
+    except AttributeError:
+        pass
+
+    return False
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
     """
     Custom permissions class for Kaznet admin users
     """
@@ -16,9 +28,26 @@ class AdminUserPermission(permissions.BasePermission):
         """
         Checks if the user in the request object is a Kaznet admin
         """
-        try:
-            return request.user.userprofile.role == UserProfile.ADMIN
-        except AttributeError:
-            pass
+        return check_admin_permission(request)
 
-        return False
+
+class IsOwnObjectOrAdmin(permissions.BasePermission):
+    """
+    Custom permissions class for Kaznet users
+    """
+    message = _('You shall not pass.')
+
+    def has_permission(self, request, view):
+        """
+        Checks if the user is an Admin when trying to list
+        """
+        if view.action in ['retrieve', 'partial_update']:
+            return True
+        return check_admin_permission(request)
+
+    def has_object_permission(self, request, view, obj):
+        """
+        Checks if the user in the request object is linked to the
+        Object
+        """
+        return request.user == obj.user or check_admin_permission(request)
