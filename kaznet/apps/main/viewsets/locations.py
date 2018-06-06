38795ee02ac0ec77ee88f2585d6c3a358fb3e4bd
DavisRayM <davisraymondmuro@outlook.com>
2018-06-06 17:40:28 +0300

    Add KaznetLocationSerializer and KaznetLocationViewSet - Add Fixtures for Tests - Add Tests for KaznetLocationSerializer and KaznetLocationViewSet

---
+++ kaznet/apps/main/viewsets/locations.py
@@ -0,0 +1,31 @@
+"""
+Main Location viewsets
+"""
+
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, mixins, viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from kaznet.apps.main.models import Location
+from kaznet.apps.main.serializers import KaznetLocationSerializer
+from kaznet.apps.users.permissions import IsAdmin
+
+
+# pylint: disable=too-many-ancestors
+class KaznetLocationViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
+                            mixins.UpdateModelMixin, viewsets.GenericViewSet,
+                            mixins.RetrieveModelMixin,
+                            mixins.DestroyModelMixin):
+    """
+    Viewset for Location
+    """
+    serializer_class = KaznetLocationSerializer
+    permission_classes = [IsAuthenticated, IsAdmin]
+    filter_backends = [
+        filters.SearchFilter,
+        DjangoFilterBackend,
+        filters.OrderingFilter]
+    filter_fields = ['parent', 'country']
+    search_fields = ['name']
+    ordering_fields = ['name', 'created']
+    queryset = Location.objects.all()
