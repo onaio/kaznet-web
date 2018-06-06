de2bfa6639bfe5350249ab76f2ec7545069175df
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-06-06 13:14:37 +0300

    Add class meta

--- kaznet/apps/main/serializers/tasks.py
+++ kaznet/apps/main/serializers/tasks.py
@@ -10,4 +10,31 @@ class KaznetTaskSerializer(TaskSerializer):
     """
     Main Task Serializer class
     """
-    model = Task
+
+    # pylint: disable=too-few-public-methods
+    class Meta(object):
+        """
+        Meta options for KaznetTaskSerializer
+        """
+        fields = [
+            'id',
+            'created',
+            'modified',
+            'name',
+            'parent',
+            'estimated_time',
+            'description',
+            'start',
+            'end',
+            'timing_rule',
+            'total_submission_target',
+            'user_submission_target',
+            'status',
+            'submission_count',
+            'target_content_type',
+            'target_id',
+            'segment_rules',
+            'locations',
+        ]
+
+        model = Task
