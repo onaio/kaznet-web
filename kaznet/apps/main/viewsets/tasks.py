8e4418e9c8b7f1896f3d7efa1c5f71c917d2793c
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-06-06 14:12:28 +0300

    Redeclare viewset class

--- kaznet/apps/main/viewsets/tasks.py
+++ kaznet/apps/main/viewsets/tasks.py
@@ -1,18 +1,36 @@
 """
 Main Tasks viewset module
 """
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
-from tasking.viewsets import TaskViewSet
 
+from kaznet.apps.main.filters import KaznetTaskFilterSet
 from kaznet.apps.main.models import Task
 from kaznet.apps.main.serializers import KaznetTaskSerializer
-from kaznet.apps.users.permissions import IsAdminOrReadOnly
 
 
-class KaznetTaskViewSet(TaskViewSet):
+# pylint: disable=too-many-ancestors
+class KaznetTaskViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
+                        mixins.RetrieveModelMixin, mixins.DestroyModelMixin,
+                        mixins.UpdateModelMixin, viewsets.GenericViewSet):
     """
     Main Task Viewset class
     """
     serializer_class = KaznetTaskSerializer
-    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+    permission_classes = [IsAuthenticated]
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.OrderingFilter,
+        filters.SearchFilter]
+    filter_class = KaznetTaskFilterSet
+    search_fields = ['name', ]
+    ordering_fields = [
+        'created',
+        'status',
+        'estimated_time',
+        'submission_count',
+        'project__id',
+        'name'
+    ]
     queryset = Task.with_submission_count.all()
