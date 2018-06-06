2934828581a5220d539bb5f5eb0390a7db9ff3f0
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-06-06 12:56:50 +0300

    Add test base

---
+++ kaznet/apps/main/tests/base.py
@@ -0,0 +1,27 @@
+"""
+Base Test class for main app
+"""
+
+from django.test import TestCase
+
+from tasking.utils import get_allowed_contenttypes
+
+
+class MainTestBase(TestCase):
+    """
+    Base test class
+    """
+
+    def setUp(self):
+        """
+        Setup tests
+        """
+        # get the content type for XForm model
+        self.xform_type = get_allowed_contenttypes().filter(
+            model='xform').first()
+        # get the content type for Instance model
+        self.xform_type = get_allowed_contenttypes().filter(
+            model='instance').first()
+        # get the content type for User model
+        self.user_type = get_allowed_contenttypes().filter(
+            model='user').first()
