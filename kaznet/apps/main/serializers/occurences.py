0978aca4f7e73749e4ba7cc7fb315d80b52607cb
DavisRayM <davisraymondmuro@outlook.com>
2018-06-07 11:50:39 +0300

    Add TaskOccurencesSerializer - Add Tests for TaskOccurenceSerializer

---
+++ kaznet/apps/main/serializers/occurences.py
@@ -0,0 +1,29 @@
+"""
+Main Occurences Serializer module
+"""
+from tasking.serializers import TaskOccurrenceSerializer
+
+from kaznet.apps.main.models import TaskOccurrence
+
+
+class KaznetTaskOccurenceSerializer(TaskOccurrenceSerializer):
+    """
+    Main Occurence Serializer
+    """
+
+    # pylint: disable=too-few-public-methods
+    class Meta(object):
+        """
+        Meta options for KaznetTaskOccurrenceSerializer
+        """
+        model = TaskOccurrence
+        fields = [
+            'id',
+            'task',
+            'created',
+            'modified',
+            'date',
+            'start_time',
+            'end_time',
+            'time_string'
+        ]
