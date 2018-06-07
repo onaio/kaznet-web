a6182258bedcd2fe01aa78472e6e1287ef84aed2
DavisRayM <davisraymondmuro@outlook.com>
2018-06-07 15:22:47 +0300

    Add Custom SerializableAmountField

--- kaznet/apps/main/serializers/bounty.py
+++ kaznet/apps/main/serializers/bounty.py
@@ -2,15 +2,41 @@
 Bounty Serializer Module
 """
 
+from django.conf import settings
+
+from django_prices.models import Money
 from rest_framework import serializers
 
 from kaznet.apps.main.models import Bounty
+
+
+class SerializableAmountField(serializers.Field):
+    """
+    Custom Field for Amount
+    """
+
+    def to_representation(self, value):
+        """
+        Custom to representation for Serializable amount field
+        """
+        return {
+            'amount': f'{value.amount}',
+            'currency': f'{value.currency}',
+        }
+
+    def to_internal_value(self, data):
+        """
+        Custom to_internal_value for SerializableAmountField
+        """
+        return Money(data, settings.KAZNET_DEFAULT_CURRENCY)
 
 
 class BountySerializer(serializers.ModelSerializer):
     """
     Client Serializer class
     """
+
+    amount = SerializableAmountField()
 
     # pylint: disable=too-few-public-methods
     class Meta(object):
