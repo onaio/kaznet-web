ae3b7e75b247ed1da4773a08bef4a1b4a90d53f4
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-06-11 16:30:51 +0300

    Add default react cors whitelist

--- kaznet/settings/local_settings.example.py
+++ kaznet/settings/local_settings.example.py
@@ -3,6 +3,8 @@ Local settings module for Kaznet
 """
 from .common import INSTALLED_APPS
 
+ALLAUTH_ONA_BASE_URL = "https://api.ona.io"
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
@@ -13,11 +15,17 @@ DATABASES = {
     }
 }
 
+# Emails
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+DEFAULT_FROM_EMAIL = 'Hello World <hello@example.com>'
+
 INSTALLED_APPS = INSTALLED_APPS + ['django_extensions', 'debug_toolbar']
 
 CORS_ORIGIN_WHITELIST = (
-    'localhost:8000',
-    '127.0.0.1:9000'
+    'localhost:3000',
+    '127.0.0.1:3000'
 )
 
 CSRF_TRUSTED_ORIGINS = CORS_ORIGIN_WHITELIST
