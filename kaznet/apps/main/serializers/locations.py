0c9cd3da0a8d70ca75df5ba6ae4d57a6545e3c1a
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-06-12 19:53:02 +0300

    redo KaznetLocationSerializer

--- kaznet/apps/main/serializers/locations.py
+++ kaznet/apps/main/serializers/locations.py
@@ -1,17 +1,55 @@
 """
 Main Location serializer module
 """
+from django_countries import Countries
 from rest_framework_json_api import serializers
-from tasking.serializers import LocationSerializer
+from tasking.common_tags import (GEODETAILS_ONLY, GEOPOINT_MISSING,
+                                 RADIUS_MISSING)
+from tasking.serializers.location import (GeopointField,
+                                          SerializableCountryField,
+                                          ShapeFileField)
 
 from kaznet.apps.main.models import Location
 
 
-class KaznetLocationSerializer(
-        serializers.ModelSerializer, LocationSerializer):
+class KaznetLocationSerializer(serializers.ModelSerializer):
     """
-    Main Location Serializer
+    KaznetLocationSerializer serializer class
     """
+    country = SerializableCountryField(
+        allow_blank=True, required=False, choices=Countries())
+
+    shapefile = ShapeFileField(required=False)
+    geopoint = GeopointField(required=False)
+
+    def validate(self, attrs):
+        """
+        Custom Validation for KaznetLocationSerializer
+        """
+        geopoint = attrs.get('geopoint')
+        radius = attrs.get('radius')
+        shapefile = attrs.get('shapefile')
+
+        if geopoint is not None:
+            if shapefile is not None:
+                raise serializers.ValidationError(
+                    {'shapefile': GEODETAILS_ONLY}
+                )
+            if radius is None:
+                raise serializers.ValidationError(
+                    {'radius': RADIUS_MISSING}
+                )
+        if radius is not None:
+            if shapefile is not None:
+                raise serializers.ValidationError(
+                    {'shapefile': GEODETAILS_ONLY}
+                )
+            if geopoint is None:
+                raise serializers.ValidationError(
+                    {'geopoint': GEOPOINT_MISSING}
+                )
+
+        return super().validate(attrs)
 
     # pylint: disable=too-few-public-methods
     class Meta(object):
