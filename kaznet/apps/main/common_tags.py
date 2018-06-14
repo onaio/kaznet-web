349c84645aef39bff4dc4f270f2735314dd513ad
DavisRayM <davisraymondmuro@outlook.com>
2018-06-14 16:50:05 +0300

    Add common_tags for Main

--- kaznet/apps/main/common_tags.py
+++ kaznet/apps/main/common_tags.py
@@ -5,3 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 
 
 INCORRECT_CLONE_DATA = _('Incorrect data.')
+INCORRECT_LOCATION = _('Submitted from wrong location')
+LACKING_EXPERTISE = _('User Expertise level does not meet Requirement')
+INVALID_SUBMISSION_TIME = _('Data Submitted at wrong time.')
+INVALID_TASK = _('Can not submit data to invalid Task.')
