0220d5ed8c0dce5c33b45b1b12b99be4cfabd131
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-07-09 18:38:04 +0300

    Update redis celery settings

--- kaznet/settings/common.py
+++ kaznet/settings/common.py
@@ -176,8 +176,8 @@ ALLAUTH_ONA_SCOPE = ['read', 'write']
 ALLAUTH_ONA_BASE_URL = ONA_BASE_URL
 
 # CELERY
-CELERY_BROKER_URL = 'redis://localhost:6379/1'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/1'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
