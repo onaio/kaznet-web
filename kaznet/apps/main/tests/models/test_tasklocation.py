eb8c7fd06a2294d356cae9c1224e8807ed07e3b2
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-07-16 11:26:17 +0300

    Add tests

--- kaznet/apps/main/tests/models/test_tasklocation.py
+++ kaznet/apps/main/tests/models/test_tasklocation.py
@@ -21,3 +21,24 @@ class TestTaskLocation(TestCase):
             location=mommy.make('main.Location', name='Kileleshwa'))
         expected = 'Coconut at Kileleshwa'
         self.assertEqual(expected, task_location.__str__())
+
+    def test_task_location_location_name(self):
+        """
+        Test the location_name property on TaskLocation model
+        """
+        task_location = mommy.make(
+            'main.TaskLocation',
+            task=mommy.make('main.Task', name='Coconut'),
+            location=mommy.make('main.Location', name='Kileleshwa'))
+        self.assertEqual('Kileleshwa', task_location.location_name)
+
+    def test_task_location_location_description(self):
+        """
+        Test the location_description property on TaskLocation model
+        """
+        task_location = mommy.make(
+            'main.TaskLocation',
+            task=mommy.make('main.Task', name='Coconut'),
+            location=mommy.make(
+                'main.Location', name='Kile', description='I love oov!'))
+        self.assertEqual('I love oov!', task_location.location_description)
