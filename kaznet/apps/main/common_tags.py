7fca9e9a154ab1860c30584e2a3e525207341bc6
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-07-16 15:50:53 +0300

    Add parent validation message

--- kaznet/apps/main/common_tags.py
+++ kaznet/apps/main/common_tags.py
@@ -5,6 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 
 
 INCORRECT_CLONE_DATA = _('Incorrect data.')
+SAME_PARENT = _('An object cannot be its own parent.')
 INCORRECT_LOCATION = _('Submitted from wrong location')
 LACKING_EXPERTISE = _('User Expertise level does not meet Requirement')
 INVALID_SUBMISSION_TIME = _('Data Submitted at wrong time.')
