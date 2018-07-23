09ca0477166e8ad19e6883e75cc9d8616a019de5
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-07-23 15:29:26 +0300

    Set last_updated_ona

--- kaznet/apps/ona/api.py
+++ kaznet/apps/ona/api.py
@@ -310,7 +310,10 @@ def process_instance(instance_data: dict, xform: object = None):
                     edited = data.get('_edited')
                     data_edited = instance_data.get('_edited')
                     last_updated = instance_data.get('_last_edited')
-                    last_updated_ona = dateutil.parser.parse(last_updated)
+
+                    last_updated_ona = None
+                    if last_updated is not None:
+                        last_updated_ona = dateutil.parser.parse(last_updated)
 
                     if (edited is not True and data_edited is True) or (
                             obj.last_updated != last_updated_ona
