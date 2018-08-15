abc1de2d3d4f1bda732024557fbb91bbc421a0ff
DavisRayM <davisraymondmuro@outlook.com>
2018-08-15 18:26:34 +0300

    Add update_details and change_password method

--- kaznet/apps/users/api.py
+++ kaznet/apps/users/api.py
@@ -1,6 +1,8 @@
 """
 API Methods For Kaznet User App
 """
+from urllib.parse import urljoin
+
 from django.conf import settings
 
 from kaznet.apps.ona.api import request_session
@@ -62,3 +64,63 @@ def add_team_member(
         added = True
 
     return (added, data)
+
+
+def update_details(
+        username: str,
+        first_name: str = None,
+        last_name: str = None,
+        email: str = None,
+        password: str = None
+):
+    """
+    Custom Method that Updates User Details
+    """
+    data = None
+
+    response = request_session(
+        urljoin(settings.ONA_BASE_URL, f'api/v1/profiles/{username}'),
+        'PATCH',
+        payload={
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': email,
+            'password': password
+        }
+    )
+
+    if response.status_code != 200:
+        data = response.json()
+        updated = False
+    else:
+        data = response.json()
+        updated = True
+
+    return (updated, data)
+
+
+def change_password(
+        username: str,
+        old_password: str,
+        password: str
+):
+    """
+    Custom Method that Changes Password
+    """
+    response = request_session(
+        urljoin(
+            settings.ONA_BASE_URL,
+            f'api/v1/profiles/{username}/change_password'),
+        'POST',
+        payload={
+            'current_password': old_password,
+            'new_password': password
+        }
+    )
+
+    if response.status_code != 204:
+        updated = False
+    else:
+        updated = True
+
+    return updated
