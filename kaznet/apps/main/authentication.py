b5a56e53ed5ce033aa750f509652950aee75adb1
DavisRayM <davisraymondmuro@outlook.com>
2018-08-20 16:11:56 +0300

    Use proper keyword on Authenticate method

--- kaznet/apps/main/authentication.py
+++ kaznet/apps/main/authentication.py
@@ -26,7 +26,7 @@ class OnaTempTokenAuthentication(TokenAuthentication):
 
         # We test for b'temptoken' since get_authorization_header
         # returns the auth in byte
-        if not auth or auth[0].lower() != self.keyword.lower().encode():
+        if not auth or auth[0].lower() != b'temptoken':
             return None
 
         if len(auth) == 1:
