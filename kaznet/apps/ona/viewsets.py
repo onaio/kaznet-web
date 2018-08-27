4d18b0d36eda5df891bb423343d0ffb0d9179361
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-08-27 15:07:59 +0300

    Add search and filter to XFormViewSet

--- kaznet/apps/ona/viewsets.py
+++ kaznet/apps/ona/viewsets.py
@@ -2,13 +2,13 @@
 Viewsets for Ona app Models
 """
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets
+from rest_framework import filters, viewsets
 from rest_framework.authentication import (SessionAuthentication,
                                            TokenAuthentication)
 from rest_framework.permissions import IsAuthenticated
 
-from kaznet.apps.ona.models import XForm
 from kaznet.apps.ona.filters import XFormFilterSet
+from kaznet.apps.ona.models import XForm
 from kaznet.apps.ona.serializers import XFormSerializer
 from kaznet.apps.users.permissions import IsAdmin
 
@@ -19,8 +19,11 @@ class XFormViewSet(viewsets.ReadOnlyModelViewSet):
     Read Only Viewset for XFormViewSet
     """
     authentication_classes = [SessionAuthentication, TokenAuthentication]
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [
+        filters.OrderingFilter, filters.SearchFilter, DjangoFilterBackend]
     filter_class = XFormFilterSet
     serializer_class = XFormSerializer
     permission_classes = [IsAuthenticated, IsAdmin]
+    search_fields = ['title']
+    ordering_fields = ['title']
     queryset = XForm.objects.all()  # pylint: disable=no-member
