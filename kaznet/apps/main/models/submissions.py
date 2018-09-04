ca644e4144ba0c172db2db219df6b81333dc7389
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-09-04 20:05:39 +0300

    Dont use relative import

--- kaznet/apps/main/models/submissions.py
+++ kaznet/apps/main/models/submissions.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.utils.translation import ugettext as _
 
 from tasking.models import BaseSubmission
-from .managers import SubmissionManager
+from kaznet.apps.main.models.managers import SubmissionManager
 
 
 class Submission(BaseSubmission):
