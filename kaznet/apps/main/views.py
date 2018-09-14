080aa0238ff6f3a2c4f6df2d20e4298b6adefbbc
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-09-14 19:10:43 +0300

    Use ContributorNotAllowedMixin in ReactAppView

--- kaznet/apps/main/views.py
+++ kaznet/apps/main/views.py
@@ -4,8 +4,11 @@ Kaznet main app views module
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import TemplateView
 
+from kaznet.apps.main.mixins import ContributorNotAllowedMixin
 
-class ReactAppView(LoginRequiredMixin, TemplateView):
+
+class ReactAppView(
+        LoginRequiredMixin, ContributorNotAllowedMixin, TemplateView):
     """
     This views is meant to serve as the frontend react app content
     https://github.com/onaio/kaznet-frontend
