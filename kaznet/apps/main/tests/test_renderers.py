235bac6869046e5a5e1ea73060371fb8608c30cf
Kahama <Kahama94@gmail.com>
2018-10-30 10:48:04 +0300

    test_renderers fixes

--- kaznet/apps/main/tests/test_renderers.py
+++ kaznet/apps/main/tests/test_renderers.py
@@ -9,7 +9,7 @@ from model_mommy import mommy
 from rest_framework.test import APIRequestFactory
 
 from kaznet.apps.main.renderers import CSVStreamingRenderer
-from kaznet.apps.main.models import Submission
+from kaznet.apps.main.models import Submission, Task, Location
 from kaznet.apps.main.serializers import SubmissionExportSerializer
 from kaznet.apps.main.tests.base import MainTestBase
 
@@ -22,12 +22,29 @@ class TestCSVStreamingRenderer(MainTestBase):
     def setUp(self):
         super().setUp()
         self.factory = APIRequestFactory()
-        task = mommy.make('main.Task', name='Quest')
+        try:
+            self.task = Task.objects.get(id=2001)
+        except Task.DoesNotExist:
+
+            task = mommy.make('main.Task', name='Quest')
+            task.id = 2001
+            task.save(force_insert=True)
+            self.task = task
+
+        try:
+            self.location = Location.objects.get(id=3001)
+        except Location.DoesNotExist:
+
+            location = mommy.make('main.Location', name='Voi')
+            location.id = 3001
+            location.save(force_insert=True)
+            self.location = location
+            self.coco_user = mommy.make('auth.User', first_name='Coco', id=1001)
         mommy.make(
             'main.Submission',
-            task=task,
-            location=mommy.make('main.Location', name='Voi'),
-            user=mommy.make('auth.User', first_name='Coco'),
+            task=self.task,
+            location=self.location,
+            user=self.coco_user,
             bounty=mommy.make(
                 'main.Bounty',
                 task=task,
@@ -37,9 +54,9 @@ class TestCSVStreamingRenderer(MainTestBase):
         )
         mommy.make(
             'main.Submission',
-            task=task,
-            location=mommy.make('main.Location', name='Voi'),
-            user=mommy.make('auth.User', first_name='Coco'),
+            task=self.task,
+            location=self.location,
+            user=self.coco_user,
             bounty=mommy.make(
                 'main.Bounty',
                 task=task,
@@ -61,7 +78,7 @@ class TestCSVStreamingRenderer(MainTestBase):
         """
         Test that we get the right data back
         """
-        expected = "id,user,user_id,task,task_id,location,location_id,submission_time,approved,status,comments,amount,currency,phone_number,payment_number\r\n555,Coco,3,Quest,2,Voi,3,2018-09-04T03:39:29+00:00,,d,,50.00,KES,,\r\n666,Coco,4,Quest,2,Voi,4,2018-09-04T03:39:29+00:00,,d,,50.00,KES,,\r\n"  # noqa
+        expected = "id,user,user_id,task,task_id,location,location_id,submission_time,approved,status,comments,amount,currency,phone_number,payment_number\r\n555,Coco,1001,Quest,2001,Voi,3001,2018-09-04T03:39:29+00:00,,d,,50.00,KES,,\r\n666,Coco,1001,Quest,2001,Voi,3001,2018-09-04T03:39:29+00:00,,d,,50.00,KES,,\r\n"  # noqa
 
         streaming_renderer = CSVStreamingRenderer()
         request = self.factory.get('/exports/submissions?format=csv')
@@ -74,6 +91,7 @@ class TestCSVStreamingRenderer(MainTestBase):
                 }
             )
         )
+
         self.assertEqual(
             expected,
             streaming_renderer_data
