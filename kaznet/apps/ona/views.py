c3d4436b90d6e490ae1182e0dbd73926c6a19d8c
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-11-15 19:17:03 +0300

    Make sure Onadata webhooks work:
    - Add appropriate parsers to webhook view
    - Fix bug in getting the request data
    - Return 200 status code instead of 201

--- kaznet/apps/ona/views.py
+++ kaznet/apps/ona/views.py
@@ -2,19 +2,22 @@
 Views Module for Ona App
 """
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, parser_classes
+from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
+from rest_framework_json_api.parsers import JSONParser as JSONAPIParser
 
 from kaznet.apps.ona.api import process_ona_webhook
 
 
 @api_view(['GET', 'POST'])
+@parser_classes([JSONParser, JSONAPIParser])
 def create_or_update_instance(request):
     """
     Creates or Updates an Instance
     """
     if request.method == 'POST':
-        data = request.data.dict()
+        data = request.data
         if process_ona_webhook(data):
-            return Response({"success": True}, status=status.HTTP_201_CREATED)
+            return Response({"success": True}, status=status.HTTP_200_OK)
     return Response({"success": False}, status=status.HTTP_400_BAD_REQUEST)
