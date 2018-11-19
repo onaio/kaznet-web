2dc811870598c457672298c402f86321b41abf2e
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-11-19 13:39:46 +0300

    Increment to version 1.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kaznet',
-    version="1.0.3",
+    version="1.0.4",
     description='Tasking application built on top of Onadata',
     license='Apache 2.0',
     author='Ona Systems Inc',
